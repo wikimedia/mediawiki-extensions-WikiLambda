76f05e5c2e48a44ccd66ec2ac8cb389c98049f6c
gengh <ggalarzaheredero@wikimedia.org>
2021-12-21 16:33:59 +0100

    Add ZFunctionCall as a possible value of an object type
    * Added ZFunctionCall class as builtin
    * Changed ZObjectFactory creation methods so that an object type can be
      expressed as a ZFunctionCall that returns a type, in adition to the
      usual ZReference to a type.
    * Added ZGenericList class as builtin function
    
    Missing:
    * T298131: More and stronger tests for ZFunctionCall and related
      ZObjectFactory tests
    * T298132: More and stronger tests for ZGenericList and related
      ZObjectFactory tests
    * T298133: Deprecate ZList as builtin type
    
    Bug: T298130
    Bug: T298127
    Bug: T297892
    Change-Id: I7d95013f598911014be075352a4c45587ad2e656

---
+++ tests/phpunit/integration/ZFunctionCallTest.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * WikiLambda integration test suite for the ZFunctionCall class
+ *
+ * @copyright 2020– WikiLambda team; see AUTHORS.txt
+ * @license MIT
+ */
+
+namespace MediaWiki\Extension\WikiLambda\Tests\Integration;
+
+use MediaWiki\Extension\WikiLambda\ZErrorException;
+use MediaWiki\Extension\WikiLambda\ZObjectFactory;
+
+/**
+ * @coversDefaultClass \MediaWiki\Extension\WikiLambda\ZObjects\ZFunctionCall
+ */
+class ZFunctionCallTest extends WikiLambdaIntegrationTestCase {
+
+	/**
+	 * @covers \MediaWiki\Extension\WikiLambda\ZObjectFactory::create
+	 * @covers ::__construct
+	 * @covers ::isValid
+	 */
+	public function testPersistentCreation() {
+		$strFunctionCall = '{"Z1K1": "Z7", "Z7K1": "Z881", "Z881K1": "Z3"}';
+		$zobject = ZObjectFactory::create( json_decode( $strFunctionCall ) );
+		$this->assertTrue( $zobject->isValid() );
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\WikiLambda\ZObjectFactory::create
+	 * @covers ::__construct
+	 * @covers ::isValid
+	 */
+	public function testGenericList() {
+		$type = <<<EOT
+{
+	"Z1K1": "Z2",
+	"Z2K1": {
+		"Z1K1": "Z6",
+		"Z6K1": "Z0"
+	},
+	"Z2K2": {
+		"Z1K1": "Z4",
+		"Z4K1": "Z0",
+		"Z4K2": {
+			"Z1K1": {
+				"Z1K1": "Z7",
+				"Z7K1": "Z881",
+				"Z881K1": "Z3"
+			},
+			"K1": {
+				"Z1K1": "Z3",
+				"Z3K1": "Z6",
+				"Z3K2": {
+					"Z1K1": "Z6",
+					"Z6K1": "Z0K1"
+				},
+				"Z3K3": {
+					"Z1K1": "Z12",
+					"Z12K1": [{
+						"Z1K1": "Z11",
+						"Z11K1": "Z1002",
+						"Z11K2": "Test key"
+					}]
+				}
+			},
+			"K2": {
+				"Z1K1": {
+					"Z1K1": "Z7",
+					"Z7K1": "Z881",
+					"Z881K1": "Z3"
+				}
+			}
+		},
+		"Z4K3": "Z101"
+	},
+	"Z2K3": {
+		"Z1K1": "Z12",
+		"Z12K1": [{
+			"Z1K1": "Z11",
+			"Z11K1": "Z1002",
+			"Z11K2": "Test type"
+		}]
+	},
+	"Z2K4": {
+		"Z1K1": "Z32",
+		"Z32K1": [{
+			"Z1K1": "Z31",
+			"Z31K1": "Z1002",
+			"Z31K2": []
+		}]
+	}
+}
+EOT;
+
+		$zobject = ZObjectFactory::create( json_decode( $type ) );
+		$this->assertTrue( $zobject->isValid() );
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\WikiLambda\ZObjectFactory::create
+	 * @covers ::__construct
+	 * @covers ::isValid
+	 */
+	public function testGenericListInner() {
+		$type = <<<EOT
+{
+	"Z1K1": "Z4",
+	"Z4K1": "Z111",
+	"Z4K2": {
+		"Z1K1": {
+			"Z1K1": "Z7",
+			"Z7K1": "Z881",
+			"Z881K1": "Z3"
+		},
+		"K1": {
+			"Z1K1": "Z3",
+			"Z3K1": "Z6",
+			"Z3K2": {
+				"Z1K1": "Z6",
+				"Z6K1": "Z111K1"
+			},
+			"Z3K3": {
+				"Z1K1": "Z12",
+				"Z12K1": [{
+					"Z1K1": "Z11",
+					"Z11K1": "Z1002",
+					"Z11K2": "Test key"
+				}]
+			}
+		},
+		"K2": {
+			"Z1K1": {
+				"Z1K1": "Z7",
+				"Z7K1": "Z881",
+				"Z881K1": "Z3"
+			}
+		}
+	},
+	"Z4K3": "Z101"
+}
+EOT;
+
+		// FIXME this returns an exception because function-schemata cannot validate generic lists yet
+		$this->expectException( ZErrorException::class );
+		$zobject = ZObjectFactory::create( json_decode( $type ) );
+		// $this->assertTrue( $zobject->isValid() );
+	}
+}
