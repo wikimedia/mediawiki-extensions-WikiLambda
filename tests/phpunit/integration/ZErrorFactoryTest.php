2592c6262b166921548536a05970ef8e2f6d7fad
James D. Forrester <jforrester@wikimedia.org>
2022-06-06 08:16:21 -0400

    Follow-up 0af102e5: Don't have ZErrorFactoryTest spam an error when doing nothing
    It's not using the tests, but now it's not erroring.
    
    Bug: T302519
    Change-Id: I99a7b498b304f853e36b3600732b8d169b3be9b6

--- tests/phpunit/integration/ZErrorFactoryTest.php
+++ tests/phpunit/integration/ZErrorFactoryTest.php
@@ -117,7 +117,7 @@ class ZErrorFactoryTest extends WikiLambdaIntegrationTestCase {
 		$testDescriptor = json_decode( SchemataUtils::readYamlAsSecretJson( $file ) );
 
 		$provide = [];
-		foreach ( $testDescriptor->test_objects->failure as $test ) {
+		foreach ( $testDescriptor->test_objects->failure ?? [] as $test ) {
 			$provide[ $test->name ] = [
 				$test->object, $test->errors ?? [], $test->php_errors ?? []
 			];
