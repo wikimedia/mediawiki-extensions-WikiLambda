eae4b7e910ca6dfa1496c0761142c57f8ba95233
temitope adesoji <adesoji@thisdot.co>
2022-09-06 09:05:56 +0100

    prevent function about page from scrolling sideways
    bug: T317081
    Change-Id: I3c1d9e300e1eb03a8027daa21a3e546037240958

--- resources/ext.wikilambda.edit/components/base/CodeEditor.vue
+++ resources/ext.wikilambda.edit/components/base/CodeEditor.vue
@@ -99,10 +99,13 @@ module.exports = exports = {
 </script>
 
 <style lang="less">
+@import './../../../lib/sd-base-variables.less';
+
 .ext-wikilambda-codeEditor {
 	width: 100%;
 	border: 1px solid #e0e0e0;
 	min-height: 85px;
 	z-index: 0;
+	.sd-box-sizing( border-box );
 }
 </style>
