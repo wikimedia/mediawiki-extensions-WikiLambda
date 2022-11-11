5393ba2d296df44d67eb5065d8224bdb73c02184
EWright <ewright-ctr@wikimedia.org>
2022-11-11 13:06:37 +0000

    Support receipt of JSON testers in the perform_test API method.
    Bug: T312290
    Bug: T321495
    Change-Id: I8a1762c3e3ee9e80731c357014c0bb3e8464d5e1

--- tests/phpunit/integration/API/ApiPerformTestTest.php
+++ tests/phpunit/integration/API/ApiPerformTestTest.php
@@ -36,6 +36,7 @@ class ApiPerformTestTest extends ApiTestCase {
 	public function addDBData() {
 		$this->insertBuiltinObjects( [ 'Z14', 'Z16', 'Z17', 'Z20', 'Z40', 'Z61', 'Z813', 'Z8130', 'Z8131', 'Z913' ] );
 		$this->editPage( 'Z1000000', $this->getTestFileContents( 'existing-zimplementation.json' ), '', NS_MAIN );
+		$this->editPage( 'Z2000000', $this->getTestFileContents( 'existing-ztester.json' ), '', NS_MAIN );
 	}
 
 	/**
@@ -45,16 +46,26 @@ class ApiPerformTestTest extends ApiTestCase {
 	 * @covers \MediaWiki\Extension\WikiLambda\API\ApiPerformTest::run
 	 */
 	public function testExecuteSuccessfully(
+		$requestedFunction,
 		$requestedZImplementations,
 		$requestedZTesters,
-		$expectedResults
+		$expectedResults,
+		$expectedError = null
 	) {
+		if ( $expectedError ) {
+		   $this->expectExceptionMessage( $expectedError );
+		}
+
 		$results = $this->doApiRequestWithToken( [
 			'action' => 'wikilambda_perform_test',
-			'wikilambda_perform_test_zfunction' => 'Z813',
+			'wikilambda_perform_test_zfunction' => $requestedFunction,
 			'wikilambda_perform_test_zimplementations' => $requestedZImplementations,
 			'wikilambda_perform_test_ztesters' => $requestedZTesters
 		] )[0]['query']['wikilambda_perform_test'];
+
+		if ( $expectedError ) {
+			return;
+		}
 
 		$this->assertEquals( count( $expectedResults ), count( $results ) );
 
@@ -63,7 +74,7 @@ class ApiPerformTestTest extends ApiTestCase {
 				$results[$i]['testMetadata']
 			);
 			$this->assertEquals(
-				'Z813',
+				$requestedFunction,
 				$results[$i]['zFunctionId']
 			);
 			$this->assertEquals(
@@ -83,6 +94,7 @@ class ApiPerformTestTest extends ApiTestCase {
 
 	public function provideExecuteSuccessfully() {
 		yield 'Request specifies implementation and tester, both by reference' => [
+			'Z813',
 			'Z913',
 			'Z8130',
 			[
@@ -94,6 +106,7 @@ class ApiPerformTestTest extends ApiTestCase {
 			]
 		];
 		yield 'Request specifies implementation by reference' => [
+			'Z813',
 			'Z913',
 			'',
 			[
@@ -110,6 +123,7 @@ class ApiPerformTestTest extends ApiTestCase {
 			]
 		];
 		yield 'Request specifies JSON for new implementation' => [
+			'Z813',
 			$this->getTestFileContents( 'new-zimplementation.json' ),
 			'',
 			[
@@ -126,6 +140,7 @@ class ApiPerformTestTest extends ApiTestCase {
 			]
 		];
 		yield 'Request specifies JSON for edited version of existing implementation' => [
+			'Z813',
 			str_replace( "True", "False", $this->getTestFileContents( 'existing-zimplementation.json' ) ),
 			'',
 			[
@@ -142,6 +157,7 @@ class ApiPerformTestTest extends ApiTestCase {
 			]
 		];
 		yield 'Request specifies tester by reference' => [
+			'Z813',
 			'',
 			'Z8130',
 			[
@@ -151,6 +167,52 @@ class ApiPerformTestTest extends ApiTestCase {
 					'validateStatus' => "{\"Z1K1\":\"Z40\",\"Z40K1\":\"Z41\"}"
 				]
 			]
+		];
+		yield 'Request specifies JSON for new tester' => [
+			'Z813',
+			'',
+			$this->getTestFileContents( 'new-ztester.json' ),
+			[
+				[
+					'zimplementationId' => 'Z913',
+					'ztesterId' => 'Z0',
+					'validateStatus' => "{\"Z1K1\":\"Z40\",\"Z40K1\":\"Z42\"}"
+				]
+			]
+		];
+		yield 'Request specifies JSON for edited version of existing tester' => [
+			'Z813',
+			'',
+			str_replace( "Z41", "Z42", $this->getTestFileContents( 'existing-ztester.json' ) ),
+			[
+				[
+					'zimplementationId' => 'Z913',
+					'ztesterId' => 'Z2000000',
+					'validateStatus' => "{\"Z1K1\":\"Z40\",\"Z40K1\":\"Z42\"}"
+				]
+			]
+		];
+		yield 'Request specifies non-existent function' => [
+			'Z123456789',
+			'',
+			'',
+			[],
+			'Perform test error: \'Z123456789\' isn\'t a known ZObject'
+		];
+		yield 'Request specifies non-existent tester' => [
+			'Z813',
+			'',
+			'Z123456789',
+			[],
+			'Perform test error: \'Z123456789\' isn\'t a known ZObject'
+		];
+		yield 'Request specifies non-tester as tester' => [
+			'Z813',
+			'',
+			'Z1000000',
+			[],
+			'Perform test error: \'{ "Z1K1": "Z14", "Z14K1": "Z813", "Z14K3": { "Z1K1": "Z16", "Z16K1": { "Z1K1": ' .
+				'"Z61", "Z61K1": "python" }, "Z16K2": "def Z813(Z813K1):\n\treturn True" } }\' isn\'t a tester.'
 		];
 	}
 }
