b6a098d1f270421fac0c417e8f754fc31a36c306
James D. Forrester <jforrester@wikimedia.org>
2023-05-03 17:24:24 -0400

    HooksTest: Update call to new SpecialRecentChanges() which has updated
    Follows-Up: Ie58c0bb2ed8ff5b0fc6d4e42256010eb825fd9dd
    Change-Id: Id67830bb3485751cdebfc40e22684fefbe75285f

--- tests/phpunit/integration/HooksTest.php
+++ tests/phpunit/integration/HooksTest.php
@@ -88,7 +88,6 @@ class HooksTest extends WikiLambdaIntegrationTestCase {
 		return new SpecialRecentChanges(
 			$this->getServiceContainer()->getWatchedItemStore(),
 			$this->getServiceContainer()->getMessageCache(),
-			$this->getServiceContainer()->getDBLoadBalancerFactory(),
 			$this->getServiceContainer()->getUserOptionsLookup()
 		);
 	}
