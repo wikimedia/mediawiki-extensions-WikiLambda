45ea169f78409f3140a4e14adc6c3bfed403759d
James D. Forrester <jforrester@wikimedia.org>
2023-07-20 11:10:46 -0400

    ZObjectAuthorizationInCreationTest: Add in some clarity on TODOs
    Change-Id: Ic13acb199d85facfb693a237ca6b798f36d0bdab

--- tests/phpunit/integration/ZObjectAuthorizationInCreationTest.php
+++ tests/phpunit/integration/ZObjectAuthorizationInCreationTest.php
@@ -47,12 +47,13 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 	 *
 	 * @param string $userType One of 'basic', 'functioneer', 'maintainer', 'sysop'
 	 * @param string $zid
-	 * @param string $content
-	 * @param array $expectedRights
-	 * @param bool $expectedAllowed
+	 * @param string $createContent
+	 * @param array $expectedCreateRights
+	 * @param bool $expectedCreateAllowed
 	 */
 	public function testCreateNew(
-		string $userType, string $zid, string $content, array $expectedRights, bool $expectedAllowed
+		string $userType, string $zid,
+		string $createContent, array $expectedCreateRights, bool $expectedCreateAllowed
 		) {
 		switch ( $userType ) {
 			case 'basic':
@@ -80,27 +81,25 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 
 		$title = Title::newFromText( $zid, NS_MAIN );
 
-		$contentObject = new ZObjectContent( $content );
+		$contentObject = new ZObjectContent( $createContent );
 
-		// Assert that the correct rights are detected
+		// Assert that the correct creation rights are detected
 		$actualRights = $this->zobjectAuthorization->getRequiredCreateRights( $contentObject, $title );
 
-		$this->assertContains( 'edit', $actualRights );
-		$this->assertContains( 'wikilambda-create', $actualRights );
-		foreach ( $expectedRights as $key => $value ) {
+		foreach ( $expectedCreateRights as $key => $value ) {
 			$this->assertContains( $value, $actualRights, "Attempted creation should require the '$value' right" );
 		}
 
 		// Attempt to make the creation
 		$attemptedCreation = $this->zobjectStore->updateZObject(
 			$zid,
-			$content,
+			$createContent,
 			'Insert new object',
 			$user,
 			EDIT_NEW
 		);
 
-		if ( $expectedAllowed ) {
+		if ( $expectedCreateAllowed ) {
 			$this->assertTrue(
 				$attemptedCreation->isOK(),
 				"A $userType user should be allowed to create this ZObject as ZID $zid."
@@ -116,7 +115,7 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 	public function provideCreateNew() {
 		$typesToTry = [
 			'type (Z4 instance)' => [
-				'content' =>
+				'createContent' =>
 					'{ "Z1K1": "Z2", "Z2K1": { "Z1K1": "Z6", "Z6K1": "Z0" }, "Z2K2": { "Z1K1": "Z4", '
 						. '"Z4K1": "Z0", '
 						. '"Z4K2": [ "Z3", '
@@ -124,17 +123,40 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 							. '{ "Z1K1": "Z12", "Z12K1": [ "Z11" ] } } ], '
 						. '"Z4K3": "Z101" }, '
 					. '"Z2K3": { "Z1K1": "Z12", "Z12K1": [ "Z11" ] } }',
-				'rights' => [ 'wikilambda-create-type' ],
-				'allowed' => [
+				'createRights' => [ 'wikilambda-create-type' ],
+				'createAllowed' => [
 					'basic' => false, 'functioneer' => true, 'maintainer' => true, 'sysop' => false
 				],
-				'allowedPredefined' => [
+				'createAllowedPredefined' => [
 					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
+				],
+				// TODO (T342357): Wire up this test rigging; currently unused
+				'modifyContent' =>
+					'{ "Z1K1": "Z2", "Z2K1": { "Z1K1": "Z6", "Z6K1": "Z0" }, "Z2K2": { "Z1K1": "Z4", '
+						. '"Z4K1": "Z0", '
+						. '"Z4K2": [ "Z3", '
+							. '{ "Z1K1": "Z3", "Z3K1": "Z40", "Z3K2": "Z0K1", "Z3K3": '
+							. '{ "Z1K1": "Z12", "Z12K1": [ "Z11" ] } } ], '
+						. '"Z4K3": "Z101" }, '
+					. '"Z2K3": { "Z1K1": "Z12", "Z12K1": [ "Z11" ] } }',
+				'modifyRights' => [ 'wikilambda-edit-type' ],
+				'modifyAllowed' => [
+					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
+				],
+				'modifyAllowedPredefined' => [
+					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
+				],
+				'labelRights' => [ 'wikilambda-edit-object-label' ],
+				'labelAllowed' => [
+					'basic' => true, 'functioneer' => true, 'maintainer' => true, 'sysop' => true
+				],
+				'labelAllowedPredefined' => [
+					'basic' => true, 'functioneer' => true, 'maintainer' => true, 'sysop' => true
 				],
 			],
 
 			'function (Z8 instance)' => [
-				'content' =>
+				'createContent' =>
 					'{ "Z1K1": "Z2", "Z2K1": { "Z1K1": "Z6", "Z6K1": "Z0" }, "Z2K2": { "Z1K1": "Z8", '
 						. '"Z8K1": [ "Z17", { "Z1K1": "Z17", '
 							. '"Z17K1": "Z6", '
@@ -145,27 +167,27 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 						. '"Z8K4": [ "Z14" ], '
 						. '"Z8K5": "Z0" }, '
 					. '"Z2K3": { "Z1K1": "Z12", "Z12K1": [ "Z11" ] } }',
-				'rights' => [ 'wikilambda-create-function' ],
-				'allowed' => [
+				'createRights' => [ 'wikilambda-create-function' ],
+				'createAllowed' => [
 					'basic' => true, 'functioneer' => true, 'maintainer' => true, 'sysop' => true
 				],
-				'allowedPredefined' => [
+				'createAllowedPredefined' => [
 					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
 				],
 			],
 
 			'implementation (Z14 instance)' => [
-				'content' =>
+				'createContent' =>
 					'{ "Z1K1": "Z2", "Z2K1": { "Z1K1": "Z6", "Z6K1": "Z0" }, "Z2K2": { "Z1K1": "Z14", '
 						. '"Z14K1": "Z801", "Z14K3": { "Z1K1": "Z16", '
 							. '"Z16K1": { "Z1K1": "Z61", "Z61K1": "Z601" }, '
 							. '"Z16K2": "function Z0( input ) {\n\treturn input;\n}" } }, '
 					. '"Z2K3": { "Z1K1": "Z12", "Z12K1": [ "Z11" ] } }',
-				'rights' => [ 'wikilambda-create-implementation' ],
-				'allowed' => [
+				'createRights' => [ 'wikilambda-create-implementation' ],
+				'createAllowed' => [
 					'basic' => true, 'functioneer' => true, 'maintainer' => true, 'sysop' => true
 				],
-				'allowedPredefined' => [
+				'createAllowedPredefined' => [
 					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
 				],
 			],
@@ -176,15 +198,15 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 			// TODO: User-defined (ZID > 10k) language (Z60 instance)
 
 			'programming language (Z61 instance)' => [
-				'content' =>
+				'createContent' =>
 					'{ "Z1K1": "Z2", "Z2K1": { "Z1K1": "Z6", "Z6K1": "Z0" }, '
 						. '"Z2K2": { "Z1K1": "Z61", "Z61K1": "test-programming-language" }, '
 						. '"Z2K3": { "Z1K1": "Z12", "Z12K1": [ "Z11" ] } } ',
-				'rights' => [ 'wikilambda-create-programming' ],
-				'allowed' => [
+				'createRights' => [ 'wikilambda-create-programming' ],
+				'createAllowed' => [
 					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
 				],
-				'allowedPredefined' => [
+				'createAllowedPredefined' => [
 					'basic' => false, 'functioneer' => false, 'maintainer' => true, 'sysop' => false
 				],
 			],
@@ -200,37 +222,38 @@ class ZObjectAuthorizationInCreationTest extends WikiLambdaIntegrationTestCase {
 		$reservedZid = 400;
 
 		foreach ( $typesToTry as $type => $attemptObject ) {
-			foreach ( $attemptObject['allowedPredefined'] ?? [] as $userType => $expectedAllowed ) {
+			foreach ( $attemptObject['createAllowedPredefined'] ?? [] as $userType => $expectedCreateAllowed ) {
+				$createContent = str_replace( 'Z0', 'Z' . $reservedZid, $attemptObject['createContent'] );
 
 				yield "Pre-defined (ZID < 10k) $type, $userType user" => [
 					$userType,
 					'Z' . $reservedZid,
-					str_replace( 'Z0', 'Z' . $reservedZid, $attemptObject['content'] ),
-					$attemptObject['rights'],
-					$expectedAllowed
+					$createContent,
+					$attemptObject['createRights'] + [ 'edit', 'wikilambda-create', 'wikilambda-create-predefined' ],
+					$expectedCreateAllowed
 				];
 
-				if ( $expectedAllowed ) {
+				if ( $expectedCreateAllowed ) {
 					$reservedZid++;
 				}
 			}
-			foreach ( $attemptObject['allowed'] ?? [] as $userType => $expectedAllowed ) {
+			foreach ( $attemptObject['createAllowed'] ?? [] as $userType => $expectedcreateAllowed ) {
 				yield "User-defined (ZID > 10k) $type, $userType user" => [
 					$userType,
 					'Z' . $userZid,
-					str_replace( 'Z0', 'Z' . $userZid, $attemptObject['content'] ),
-					$attemptObject['rights'],
-					$expectedAllowed
+					str_replace( 'Z0', 'Z' . $userZid, $attemptObject['createContent'] ),
+					$attemptObject['createRights'] + [ 'edit', 'wikilambda-create' ],
+					$expectedcreateAllowed
 				];
 
-				if ( $expectedAllowed ) {
+				if ( $expectedcreateAllowed ) {
 					$userZid++;
 				}
 			}
 		}
 	}
 
-	// TODO: Edits to pre-existing content, especially:
+	// TODO (T342357): Edits to pre-existing content, especially:
 	// TODO: Pre-defined (ZID < 10k) label change
 	// TODO: User-defined (ZID > 10k) label change
 }
