45fd00d42970b483254d9f1438944771fc8b5393
gengh <ggalarzaheredero@wikimedia.org>
2023-07-26 10:38:56 +0200

    Fix and activate Run Function page integration tests
    Bug: T318416
    Change-Id: I8e92e0eb63eaefc0b856f17d5e3216c40aa2f96f

---
+++ tests/jest/integration/RunFunctionCall.test.js
@@ -0,0 +1,117 @@
+/*!
+ * WikiLambda integration test for evaluating a function call on the Special: Evaluate Function Call page.
+ *
+ * @copyright 2020– Abstract Wikipedia team; see AUTHORS.txt
+ * @license MIT
+ */
+'use strict';
+
+require( '@testing-library/jest-dom' );
+
+const { fireEvent, render } = require( '@testing-library/vue' ),
+	{ within } = require( '@testing-library/dom' ),
+	{ clickLookupResult } = require( './helpers/interactionHelpers.js' ),
+	{ runSetup, runTeardown } = require( './helpers/runFunctionTestHelpers.js' ),
+	store = require( '../../../resources/ext.wikilambda.edit/store/index.js' ),
+	App = require( '../../../resources/ext.wikilambda.edit/components/App.vue' ),
+	expectedFunctionCallPostedToApi = require( './objects/expectedFunctionCallPostedToApi.js' );
+
+describe( 'Wikilambda frontend, running a function on Run Function Special page', () => {
+	let apiPostWithFunctionCallMock;
+
+	beforeEach( () => {
+		const setupResult = runSetup();
+		apiPostWithFunctionCallMock = setupResult.apiPostWithFunctionCallMock;
+	} );
+
+	afterEach( () => {
+		runTeardown();
+	} );
+
+	it( 'allows choosing a function and calling it', async () => {
+		const { findByRole, findByText, findByTestId } =
+			render( App, { global: { plugins: [ store ] } } );
+
+		// ACT: Select a function
+		const functionSelectorBlock = await findByTestId( 'function-evaluator-call' );
+		const functionSelector = within( functionSelectorBlock ).getByRole( 'combobox' );
+		const functionName = 'function name, in Chinese';
+
+		await fireEvent.update( functionSelector, functionName );
+		await clickLookupResult( functionSelectorBlock, functionName );
+
+		// ASSERT: The function is selected as the function to call.
+		expect( functionSelector.value ).toBe( functionName );
+
+		// ASSERT: Confirm whether the function's two arguments are displayed
+		const functionInputsBlock = await findByTestId( 'function-evaluator-inputs' );
+		const functionInputs = await within( functionInputsBlock ).getAllByTestId( 'z-object-key-value' );
+		expect( functionInputs ).toHaveLength( 2 );
+
+		//* -- First Input
+		// ACT: Expand first input
+		const firstInput = functionInputs[ 0 ];
+		const firstInputExpandToggle = await within( firstInput ).getByTestId( 'expanded-toggle' );
+		await fireEvent.click( firstInputExpandToggle );
+
+		// ASSERT: First input is displayed with correct type.
+		const firstInputTypeSelect = await within( firstInput ).getByTestId( 'z-object-type-select' );
+		expect( within( firstInputTypeSelect ).getByRole( 'combobox' ) ).toHaveTextContent( 'String' );
+
+		// ACT: Enter a value for the first input
+		const firstInputTextField = await within( firstInput ).getByTestId( 'text-input' );
+		const firstInputText = 'first argument value';
+		await fireEvent.update( firstInputTextField, firstInputText );
+
+		// ASSERT: Check the value of the first input is correctly set
+		expect( firstInputTextField.value ).toBe( firstInputText );
+
+		//* -- Second Input
+		// ACT: Expand second input
+		const secondInput = functionInputs[ 1 ];
+		const secondInputExpandToggle = await within( secondInput ).getByTestId( 'expanded-toggle' );
+		await fireEvent.click( secondInputExpandToggle );
+
+		// ASSERT: second input is displayed with correct type.
+		const secondInputTypeSelect = await within( secondInput ).getByTestId( 'z-object-type-select' );
+		expect( within( secondInputTypeSelect ).getByRole( 'combobox' ) ).toHaveTextContent( 'String' );
+
+		// ACT: Enter a value for the second input
+		const secondInputTextField = await within( secondInput ).getByTestId( 'text-input' );
+		const secondInputText = 'second argument value';
+		await fireEvent.update( secondInputTextField, secondInputText );
+
+		// ASSERT: Check the value of the second input is correctly set
+		expect( secondInputTextField.value ).toBe( secondInputText );
+
+		//* -- Calling the function
+		// ACT: Click the Run Function button.
+		const runFunctionButton = await findByRole( 'button', { name: 'Run function' } );
+		await fireEvent.click( runFunctionButton );
+
+		// ASSERT: The correct function call is sent to the API with the newly input values.
+		expect( apiPostWithFunctionCallMock ).toHaveBeenCalledWith( {
+			action: 'wikilambda_function_call',
+			wikilambda_function_call_zobject: JSON.stringify( expectedFunctionCallPostedToApi )
+		} );
+
+		//* -- Checking the response
+		const resultBlock = await findByTestId( 'function-evaluator-result' );
+
+		// ASSERT: The 'Running...' message is displayed.
+		expect( await within( resultBlock ).findByText( 'Running...' ) ).toBeInTheDocument();
+
+		// ASSERT: Eventually the result is displayed
+		expect( await findByText( 'the function call result' ) ).toBeInTheDocument();
+
+		// ACT: Click the show metrics button.
+		const detailsLink = await within( resultBlock ).findByRole( 'button', { name: 'Details' } );
+		await fireEvent.click( detailsLink );
+
+		// ASSERT: The metadata is displayed in the dialog.
+		const detailsDialog = await findByRole( 'dialog' );
+		expect( detailsDialog ).toHaveTextContent( 'Orchestration start time: 11 seconds ago' );
+		expect( detailsDialog ).toHaveTextContent( 'Orchestration end time: 2 seconds ago' );
+		expect( detailsDialog ).toHaveTextContent( 'Orchestration duration: 146ms' );
+	} );
+} );
---
+++ tests/jest/integration/helpers/runFunctionTestHelpers.js
@@ -0,0 +1,96 @@
+/*!
+ * WikiLambda unit test suite run function page helper.
+ *
+ * @copyright 2020– Abstract Wikipedia team; see AUTHORS.txt
+ * @license MIT
+ */
+'use strict';
+
+const functionCallResultFromApi = require( '../objects/functionCallResultFromApi.js' ),
+	Constants = require( '../../../../resources/ext.wikilambda.edit/Constants.js' ),
+	apiGetMock = require( './apiGetMock.js' ),
+	ApiMock = require( './apiMock.js' );
+
+const lookupZObjectTypeLabels = new ApiMock(
+	apiGetMock.functionLabelsRequest,
+	apiGetMock.labelsResponse,
+	apiGetMock.labelsMatcher
+);
+
+const initializeRootZObject = new ApiMock(
+	apiGetMock.loadZObjectsRequest,
+	apiGetMock.loadZObjectsResponse,
+	apiGetMock.loadZObjectsMatcher
+);
+
+const runSetup = function () {
+	// Needed because of the Teleported component.
+	const el = document.createElement( 'div' );
+	el.id = 'ext-wikilambda-app';
+	document.body.appendChild( el );
+
+	jest.useFakeTimers().setSystemTime( new Date( '2022-11-09T19:56:53Z' ) );
+
+	// Set page url and mw.Uri mock
+	Object.defineProperty( window, 'location', {
+		value: {
+			href: 'currentPage'
+		}
+	} );
+	window.mw.Uri = jest.fn( () => {
+		return {
+			path: new window.mw.Title( Constants.PATHS.RUN_FUNCTION_TITLE ).getUrl(),
+			query: {}
+		};
+	} );
+
+	// Set mw.config variables
+	global.mw.config.get = jest.fn( ( endpoint ) => {
+		switch ( endpoint ) {
+			case 'wgWikiLambda':
+				return {
+					runFunction: true,
+					viewmode: false
+				};
+			default:
+				return {};
+		}
+	} );
+
+	// Set wiki url encode mock
+	mw.internalWikiUrlencode = jest.fn( ( url ) => url );
+
+	// Set function call API mocks
+	const apiPostWithFunctionCallMock = jest.fn( () => Promise.resolve( {
+		query: {
+			wikilambda_function_call: {
+				success: '',
+				data: JSON.stringify( functionCallResultFromApi )
+			}
+		}
+	} ) );
+	mw.Api = jest.fn( () => {
+		return {
+			post: apiPostWithFunctionCallMock,
+			get: apiGetMock.createMockApi( [
+				lookupZObjectTypeLabels,
+				initializeRootZObject
+			] )
+		};
+	} );
+
+	return {
+		apiPostWithFunctionCallMock: apiPostWithFunctionCallMock
+	};
+};
+
+const runTeardown = () => {
+	document.body.outerHTML = '';
+	jest.runOnlyPendingTimers();
+	jest.useRealTimers();
+};
+
+module.exports = {
+	runSetup: runSetup,
+	runTeardown: runTeardown
+};
