55d712ff318b0ff6d1e376a7a82dadb3ccd90813
James D. Forrester <jforrester@wikimedia.org>
2024-01-18 17:05:30 -0500

    tests: Fill in some coverage for ZError
    Bug: T302599
    Change-Id: I8c5cc45eb27212be471b2e45bfcee95ac7e1fbd5

--- tests/phpunit/integration/ZErrorTest.php
+++ tests/phpunit/integration/ZErrorTest.php
@@ -9,6 +9,7 @@
 
 namespace MediaWiki\Extension\WikiLambda\Tests\Integration;
 
+use MediaWiki\Extension\WikiLambda\ZObjectFactory;
 use MediaWiki\Extension\WikiLambda\ZObjects\ZError;
 use MediaWiki\Extension\WikiLambda\ZObjects\ZObject;
 use MediaWiki\Extension\WikiLambda\ZObjects\ZReference;
@@ -19,6 +20,25 @@ use MediaWiki\Extension\WikiLambda\ZObjects\ZString;
  * @group Database
  */
 class ZErrorTest extends WikiLambdaIntegrationTestCase {
+
+	public function testCreation_factory() {
+		$stringZObject = <<<EOT
+{
+	"Z1K1": "Z5",
+	"Z5K1": "Z501",
+	"Z5K2": "error message"
+}
+EOT;
+
+		$testObject = ZObjectFactory::create( json_decode( $stringZObject ) );
+
+		$this->assertSame( 'Z5', $testObject->getZType() );
+
+		$this->assertSame(
+			[ 'Z1K1' => 'Z5', 'Z5K1' => 'Z501', 'Z5K2' => 'error message' ],
+			(array)$testObject->getSerialized()
+		);
+	}
 
 	public function testCreate_invalidErrorType() {
 		$testObject = new ZError( 'invalid', new ZString( 'error message' ) );
@@ -44,6 +64,31 @@ class ZErrorTest extends WikiLambdaIntegrationTestCase {
 		$testObject = new ZError( 'Z501', new ZString( 'error message' ) );
 		$this->assertTrue( $testObject->isValid() );
 		$this->assertSame( 'Z501', $testObject->getZErrorType() );
+
+		$testObjectErrorData = $testObject->getErrorData();
+
+		// NOTE: This message label is "wrong" because we don't load Z501 in this test run
+		$this->assertSame( 'Unknown error Z501', $testObject->getMessage() );
+		$this->assertSame( 'Unknown error Z501', $testObjectErrorData['message'] );
+
+		$this->assertArrayEquals(
+			[ 'Z1K1' => 'Z5', 'Z5K1' => 'Z501', 'Z5K2' => 'error message' ],
+			(array)$testObjectErrorData['zerror'],
+			"Getting the 'zerror' from getErrorData() returns the input"
+		);
+
+		// NOTE: This is identical to 'zerror' as we don't load the labels in this test run"
+		$this->assertArrayEquals(
+			[ 'Z1K1' => 'Z5', 'Z5K1' => 'Z501', 'Z5K2' => 'error message' ],
+			(array)$testObjectErrorData['labelled'],
+			"Getting the 'labelled' from getErrorData() returns the 'transformed' input"
+		);
+
+		$this->assertSame(
+			[ 'Z1K1' => 'Z5', 'Z5K1' => 'Z501', 'Z5K2' => 'error message' ],
+			(array)$testObject->getSerialized()
+		);
+
 		$this->assertInstanceOf( ZObject::class, $testObject->getZValue() );
 	}
 }
