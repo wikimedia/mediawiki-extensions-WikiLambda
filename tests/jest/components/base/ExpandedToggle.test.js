243821544ca5f4f15e4fd22165c76b41c0b24453
Grace C <gchoi@wikimedia.org>
2024-04-03 21:18:30 -0700

    Add test for ExpandedToggle component
    Bug: T358207
    Change-Id: Ib6b4a786368e712d9ba69dbc5f205575d3456cd2

---
+++ tests/jest/components/base/ExpandedToggle.test.js
@@ -0,0 +1,93 @@
+/*!
+ * WikiLambda unit test suite for the default ExpandedToggle component.
+ *
+ * @copyright 2020– Abstract Wikipedia team; see AUTHORS.txt
+ * @license MIT
+ */
+'use strict';
+
+var shallowMount = require( '@vue/test-utils' ).shallowMount,
+	{ waitFor } = require( '@testing-library/vue' ),
+	createGetterMock = require( '../../helpers/getterHelpers.js' ).createGetterMock,
+	ExpandedToggle = require( '../../../../resources/ext.wikilambda.edit/components/base/ExpandedToggle.vue' );
+
+describe( 'ExpandedToggle', () => {
+	beforeEach( () => {
+		getters = {
+			waitForRunningParsers: createGetterMock( Promise.resolve() )
+		};
+		global.store.hotUpdate( { getters: getters } );
+	} );
+
+	describe( 'when toggle is enabled', () => {
+		it( 'renders without errors', () => {
+			const wrapper = shallowMount( ExpandedToggle, {
+				props: {
+					expanded: true,
+					hasExpandedMode: true // togglable
+				},
+				global: { stubs: { CdxButton: false } }
+			} );
+
+			expect( wrapper.findComponent( { name: 'wl-expanded-toggle' } ).exists() ).toBe( true );
+		} );
+
+		it( 'expands when collapsed and button is clicked', async () => {
+			const wrapper = shallowMount( ExpandedToggle, {
+				props: {
+					expanded: false, // collapsed
+					hasExpandedMode: true
+				},
+				global: { stubs: { CdxButton: false } }
+			} );
+
+			expect( wrapper.find( '.ext-wikilambda-expand-toggle-collapsed' ).exists() ).toBe( true );
+
+			wrapper.findComponent( { name: 'cdx-button' } ).trigger( 'click' );
+
+			await waitFor( () => expect( wrapper.emitted( 'toggle' ) ).toBeTruthy() );
+		} );
+
+		it( 'collapses when expanded and button is clicked', async () => {
+			const wrapper = shallowMount( ExpandedToggle, {
+				props: {
+					expanded: true, // expanded
+					hasExpandedMode: true
+				},
+				global: { stubs: { CdxButton: false } }
+			} );
+
+			expect( wrapper.find( '.ext-wikilambda-expand-toggle-expanded' ).exists() ).toBe( true );
+
+			wrapper.findComponent( { name: 'cdx-button' } ).trigger( 'click' );
+
+			await waitFor( () => expect( wrapper.emitted( 'toggle' ) ).toBeTruthy() );
+		} );
+	} );
+
+	describe( 'when toggle is disabled', () => {
+		it( 'renders without errors', () => {
+			const wrapper = shallowMount( ExpandedToggle, {
+				props: {
+					expanded: false,
+					hasExpandedMode: false // not togglable
+				},
+				global: { stubs: { CdxButton: false } }
+			} );
+
+			expect( wrapper.findComponent( { name: 'wl-expanded-toggle' } ).exists() ).toBe( true );
+		} );
+
+		it( 'shows a non-collapse/expandable button', () => {
+			const wrapper = shallowMount( ExpandedToggle, {
+				props: {
+					expanded: false,
+					hasExpandedMode: false
+				},
+				global: { stubs: { CdxButton: false } }
+			} );
+
+			expect( wrapper.find( '.ext-wikilambda-expand-toggle-disabled' ).exists() ).toBe( true );
+		} );
+	} );
+} );
