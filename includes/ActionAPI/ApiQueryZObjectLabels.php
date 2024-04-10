fb81a35053d7b8c90ae1c40f7a46b0fdd0d1e18e
James D. Forrester <jforrester@wikimedia.org>
2024-04-10 13:59:35 -0400

    Action APIs: Set most of our APIs to emit a cache header for 24 hours
    Note that MediaWiki out-guesses us and over-rides the 'public' value
    back to 'anon-public-user-private', which is at least better than it
    was as 'private'.
    
    Not setting a cache for the perform-test API end-point, as those are
    not expected to be run often anyway (as the client is inside the PHP
    code for WikiLambda, and cached into the database there).
    
    Setting the public function call result cache value to 7 days, not 1.
    
    Bug: T362271
    Change-Id: Ife1893822a44f2523d376ac9eef773fa3191bd8f

--- includes/ActionAPI/ApiQueryZObjectLabels.php
+++ includes/ActionAPI/ApiQueryZObjectLabels.php
@@ -34,6 +34,11 @@ class ApiQueryZObjectLabels extends ApiQueryGeneratorBase {
 	 * @inheritDoc
 	 */
 	public function execute() {
+		// (T362271) Emit appropriate cache headers for a 24 hour TTL
+		// NOTE (T362273): MediaWiki out-guesses us and assumes we don't know what we're doing; to fix so it works
+		$this->getMain()->setCacheMode( 'public' );
+		$this->getMain()->setCacheMaxAge( 60 * 60 * 24 );
+
 		$this->run();
 	}
 
