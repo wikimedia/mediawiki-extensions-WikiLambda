a8d8dd1446ad1d5e0b97e6a70dfc5472ad338598
gengh <ggalarzaheredero@wikimedia.org>
2025-05-16 19:10:18 +0200

    Overwrite LiteralStringPFragment::asDom for empty values
    Bug: T391589
    Change-Id: Ide0079bce17c09072e6b65694a7aa0c1e6108ec7

--- includes/ParserFunction/WikifunctionsPFragment.php
+++ includes/ParserFunction/WikifunctionsPFragment.php
@@ -11,8 +11,27 @@
 
 namespace MediaWiki\Extension\WikiLambda\ParserFunction;
 
+use Wikimedia\Parsoid\DOM\DocumentFragment;
+use Wikimedia\Parsoid\Ext\ParsoidExtensionAPI;
 use Wikimedia\Parsoid\Fragments\LiteralStringPFragment;
 
 class WikifunctionsPFragment extends LiteralStringPFragment {
 	// TODO: Alter this so we can control serialisation etc.
+
+	/**
+	 * @inheritDoc
+	 */
+	public function asDom( ParsoidExtensionAPI $ext, bool $release = false ): DocumentFragment {
+		if ( !$this->isEmpty() ) {
+			return parent::asDom( $ext, $release );
+		}
+
+		// (T391589) LiteralStringPFragment::asDom fails when the value is empty,
+		// adding excecption here to create an empty text node.
+		$doc = $ext->getTopLevelDoc();
+		$df = $doc->createDocumentFragment();
+		$df->appendChild( $doc->createTextNode( '' ) );
+
+		return $df;
+	}
 }
