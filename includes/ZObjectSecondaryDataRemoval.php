7d67c8d6fd7e37bb005fe84d48a538f5b62188d8
Daphne Smit <dsmit@wikimedia.org>
2025-06-17 12:56:33 +0200

    When deleting a ZObject, check that if it's an approved Implementation/Tester and disconnect it
    Refactor ZObjectSecondaryDataRemoval:
    
    - Store ZObjectStore as a private property.
    - Add a new method 'removeFunctionReferenceIfNeeded' to handle the removal of function references when a tester or implementation is deleted.
      - This method will check if the zid that is removed is an Implementation or Tester and remove the reference to it in the function and then save it back to the DB.
      - this will show up in the history of the function as for example: Remove reference to deleted Z14 with ZID Z23791.
    - Add a new method to the ZObjectStore class 'findFunctionsReferencingZObjectByKey' to find functions that reference a ZObject by its key.
    
    Bug: T392160
    Bug: T383502
    Change-Id: Ia6b34552b21b058975a69388e12e1a6af09bfe04

--- includes/ZObjectSecondaryDataRemoval.php
+++ includes/ZObjectSecondaryDataRemoval.php
@@ -12,34 +12,53 @@ namespace MediaWiki\Extension\WikiLambda;
 
 use MediaWiki\Deferred\DataUpdate;
 use MediaWiki\Extension\WikiLambda\Registry\ZObjectRegistry;
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\Title\Title;
+use Psr\Log\LoggerInterface;
 
 class ZObjectSecondaryDataRemoval extends DataUpdate {
 
 	private Title $title;
+	private ZObjectStore $zObjectStore;
+	private LoggerInterface $logger;
 
 	/**
 	 * @inheritDoc
 	 */
 	public function __construct( Title $title ) {
 		$this->title = $title;
+		$this->zObjectStore = WikiLambdaServices::getZObjectStore();
+		$this->logger = LoggerFactory::getInstance( 'WikiLambda' );
 	}
 
+	/**
+	 * Main entry point for removing all secondary data related to a ZObject.
+	 *
+	 * @return void
+	 */
 	public function doUpdate() {
 		$zid = $this->title->getDBkey();
 
-		$zObjectStore = WikiLambdaServices::getZObjectStore();
+		// Remove all secondary store data for the given ZID.
+		$this->zObjectStore->deleteZObjectLabelsByZid( $zid );
+		$this->zObjectStore->deleteZObjectLabelConflictsByZid( $zid );
+		$this->zObjectStore->deleteZFunctionReference( $zid );
+		$this->zObjectStore->deleteRelatedZObjects( $zid );
+		$this->zObjectStore->deleteZLanguageFromLanguagesCache( $zid );
 
-		$zObjectStore->deleteZObjectLabelsByZid( $zid );
-		$zObjectStore->deleteZObjectLabelConflictsByZid( $zid );
-		$zObjectStore->deleteZFunctionReference( $zid );
-		$zObjectStore->deleteRelatedZObjects( $zid );
-		$zObjectStore->deleteZLanguageFromLanguagesCache( $zid );
+		// If the ZObject is an implementation or tester, remove its reference from the function.
+		$this->zObjectStore->removeFunctionReferenceIfImplementationOrTester( $zid );
 
-		// Unregister the zid from any of the type-specific caches
+		// Remove all rows where the deleted ZObject is the related_zobject.
+		$this->zObjectStore->deleteRelatedZObjects( null, null, null, $zid );
+
+		// Clear tester result caches for this ZID.
+		$this->zObjectStore->deleteZFunctionFromZTesterResultsCache( $zid );
+		$this->zObjectStore->deleteZImplementationFromZTesterResultsCache( $zid );
+		$this->zObjectStore->deleteZTesterFromZTesterResultsCache( $zid );
+
+		// Unregister the ZID from caches and clear object cache.
 		ZObjectRegistry::unregisterZid( $zid );
-
-		// Drop the ZObject from the object cache, if set
 		$zObjectCache = WikiLambdaServices::getZObjectStash();
 		$cacheKey = $zObjectCache->makeKey( ZObjectStore::ZOBJECT_CACHE_KEY_PREFIX, $zid );
 		$zObjectCache->delete( $cacheKey );
