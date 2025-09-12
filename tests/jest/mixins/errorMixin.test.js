e7d304966f62519d3a4a1449023665f66c891705
Daphne Smit <dsmit@wikimedia.org>
2025-09-12 14:16:51 -0400

    errorMixin.test.js: Distinguish method and computed property tests
    Change-Id: I5c7bfacd77b453a3bc7af358c0f5fb53a579331d

--- tests/jest/mixins/errorMixin.test.js
+++ tests/jest/mixins/errorMixin.test.js
@@ -43,190 +43,194 @@ describe( 'errorMixin mixin', () => {
 		wrapper = shallowMount( TestComponent );
 	} );
 
-	describe( 'clearFieldErrors', () => {
-		it( 'should call clearErrors action if keyPath is defined and is not main', () => {
-			wrapper.vm.keyPath = 'main.Z2K2';
-			wrapper.vm.clearFieldErrors();
+	describe( 'methods', () => {
+		describe( 'clearFieldErrors', () => {
+			it( 'should call clearErrors action if keyPath is defined and is not main', () => {
+				wrapper.vm.keyPath = 'main.Z2K2';
+				wrapper.vm.clearFieldErrors();
 
-			expect( store.clearErrors ).toHaveBeenCalledWith( 'main.Z2K2' );
-		} );
-
-		it( 'should not call clearErrors action if keyPath is not defined', () => {
-			wrapper.vm.keyPath = undefined;
-			wrapper.vm.clearFieldErrors();
-
-			expect( store.clearErrors ).not.toHaveBeenCalled();
-		} );
-
-		it( 'should not call clearErrors action if keyPath is main', () => {
-			wrapper.vm.keyPath = 'main';
-			wrapper.vm.clearFieldErrors();
-
-			expect( store.clearErrors ).not.toHaveBeenCalled();
-		} );
-	} );
-
-	describe( 'getErrorMessage', () => {
-		it( 'should return the error message from error object', () => {
-			const error = { message: 'Error occurred' };
-			expect( wrapper.vm.getErrorMessage( error ) ).toBe( 'Error occurred' );
-		} );
-	} );
-
-	describe( 'setErrorMessageCallback', () => {
-		it( 'calls callback with fallback message if there is no metadata', () => {
-			const fallbackMsg = 'fallback error message';
-			const callback = jest.fn();
-			wrapper.vm.setErrorMessageCallback( null, fallbackMsg, callback );
-			expect( callback ).toHaveBeenCalledWith( fallbackMsg );
-		} );
-
-		it( 'calls callback with fallback message if metadata has no errors', () => {
-			const fallbackMsg = 'fallback error message';
-			const callback = jest.fn();
-			wrapper.vm.setErrorMessageCallback( metadataBasic, fallbackMsg, callback );
-			expect( callback ).toHaveBeenCalledWith( fallbackMsg );
-		} );
-
-		it( 'calls callback with generic error message (global keys)', () => {
-			const callback = jest.fn();
-			const fallbackMsg = 'fallback error message';
-			const metadata = convertSetToMap( { errors: {
-				Z1K1: 'Z5',
-				Z5K1: 'Z500',
-				Z5K2: {
-					Z1K1: {
-						Z1K1: 'Z7',
-						Z7K1: 'Z885',
-						Z885K1: 'Z500'
-					},
-					Z500K1: 'some generic error message'
-				}
-			} } );
-
-			wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
-			expect( callback ).toHaveBeenCalledWith( 'some generic error message' );
-		} );
-
-		it( 'calls callback with generic error message (local keys)', () => {
-			const callback = jest.fn();
-			const fallbackMsg = 'fallback error message';
-			const metadata = convertSetToMap( { errors: {
-				Z1K1: 'Z5',
-				Z5K1: 'Z500',
-				Z5K2: {
-					Z1K1: {
-						Z1K1: 'Z7',
-						Z7K1: 'Z885',
-						Z885K1: 'Z500'
-					},
-					K1: 'some generic error message'
-				}
-			} } );
-
-			wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
-			expect( callback ).toHaveBeenCalledWith( 'some generic error message' );
-		} );
-
-		it( 'calls callback with fallback error message when generic error has non-string key', async () => {
-			const callback = jest.fn();
-			const fallbackMsg = 'fallback error message';
-			const metadata = convertSetToMap( { errors: {
-				Z1K1: 'Z5',
-				Z5K1: 'Z500',
-				Z5K2: {
-					Z1K1: {
-						Z1K1: 'Z7',
-						Z7K1: 'Z885',
-						Z885K1: 'Z500'
-					},
-					Z500K1: {
-						Z1K1: 'Z99',
-						Z99K1: 1
-					}
-				}
-			} } );
-
-			wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
-			await waitFor( () => expect( callback ).toHaveBeenCalledWith( fallbackMsg ) );
-		} );
-
-		it( 'calls callback with custom error zid was successfully fetched', async () => {
-			store.getStoredObject = jest.fn().mockReturnValue( {
-				Z1K1: 'Z2',
-				Z2K1: { Z1K1: 'Z6', Z6K1: 'Z1500' },
-				Z2K2: { Z1K1: 'Z50' }
+				expect( store.clearErrors ).toHaveBeenCalledWith( 'main.Z2K2' );
 			} );
 
-			const callback = jest.fn();
-			const fallbackMsg = 'fallback error message';
-			const metadata = convertSetToMap( { errors: {
-				Z1K1: 'Z5',
-				Z5K1: 'Z1500',
-				Z5K2: {
-					Z1K1: {
-						Z1K1: 'Z7',
-						Z7K1: 'Z885',
-						Z885K1: 'Z1500'
-					},
-					Z1500K1: 'one arg',
-					Z1500K2: 'another arg'
-				}
-			} } );
+			it( 'should not call clearErrors action if keyPath is not defined', () => {
+				wrapper.vm.keyPath = undefined;
+				wrapper.vm.clearFieldErrors();
 
-			wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
-			await waitFor( () => expect( callback ).toHaveBeenCalledWith( 'Custom error' ) );
+				expect( store.clearErrors ).not.toHaveBeenCalled();
+			} );
+
+			it( 'should not call clearErrors action if keyPath is main', () => {
+				wrapper.vm.keyPath = 'main';
+				wrapper.vm.clearFieldErrors();
+
+				expect( store.clearErrors ).not.toHaveBeenCalled();
+			} );
+		} );
+
+		describe( 'getErrorMessage', () => {
+			it( 'should return the error message from error object', () => {
+				const error = { message: 'Error occurred' };
+				expect( wrapper.vm.getErrorMessage( error ) ).toBe( 'Error occurred' );
+			} );
+		} );
+
+		describe( 'setErrorMessageCallback', () => {
+			it( 'calls callback with fallback message if there is no metadata', () => {
+				const fallbackMsg = 'fallback error message';
+				const callback = jest.fn();
+				wrapper.vm.setErrorMessageCallback( null, fallbackMsg, callback );
+				expect( callback ).toHaveBeenCalledWith( fallbackMsg );
+			} );
+
+			it( 'calls callback with fallback message if metadata has no errors', () => {
+				const fallbackMsg = 'fallback error message';
+				const callback = jest.fn();
+				wrapper.vm.setErrorMessageCallback( metadataBasic, fallbackMsg, callback );
+				expect( callback ).toHaveBeenCalledWith( fallbackMsg );
+			} );
+
+			it( 'calls callback with generic error message (global keys)', () => {
+				const callback = jest.fn();
+				const fallbackMsg = 'fallback error message';
+				const metadata = convertSetToMap( { errors: {
+					Z1K1: 'Z5',
+					Z5K1: 'Z500',
+					Z5K2: {
+						Z1K1: {
+							Z1K1: 'Z7',
+							Z7K1: 'Z885',
+							Z885K1: 'Z500'
+						},
+						Z500K1: 'some generic error message'
+					}
+				} } );
+
+				wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
+				expect( callback ).toHaveBeenCalledWith( 'some generic error message' );
+			} );
+
+			it( 'calls callback with generic error message (local keys)', () => {
+				const callback = jest.fn();
+				const fallbackMsg = 'fallback error message';
+				const metadata = convertSetToMap( { errors: {
+					Z1K1: 'Z5',
+					Z5K1: 'Z500',
+					Z5K2: {
+						Z1K1: {
+							Z1K1: 'Z7',
+							Z7K1: 'Z885',
+							Z885K1: 'Z500'
+						},
+						K1: 'some generic error message'
+					}
+				} } );
+
+				wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
+				expect( callback ).toHaveBeenCalledWith( 'some generic error message' );
+			} );
+
+			it( 'calls callback with fallback error message when generic error has non-string key', async () => {
+				const callback = jest.fn();
+				const fallbackMsg = 'fallback error message';
+				const metadata = convertSetToMap( { errors: {
+					Z1K1: 'Z5',
+					Z5K1: 'Z500',
+					Z5K2: {
+						Z1K1: {
+							Z1K1: 'Z7',
+							Z7K1: 'Z885',
+							Z885K1: 'Z500'
+						},
+						Z500K1: {
+							Z1K1: 'Z99',
+							Z99K1: 1
+						}
+					}
+				} } );
+
+				wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
+				await waitFor( () => expect( callback ).toHaveBeenCalledWith( fallbackMsg ) );
+			} );
+
+			it( 'calls callback with custom error zid was successfully fetched', async () => {
+				store.getStoredObject = jest.fn().mockReturnValue( {
+					Z1K1: 'Z2',
+					Z2K1: { Z1K1: 'Z6', Z6K1: 'Z1500' },
+					Z2K2: { Z1K1: 'Z50' }
+				} );
+
+				const callback = jest.fn();
+				const fallbackMsg = 'fallback error message';
+				const metadata = convertSetToMap( { errors: {
+					Z1K1: 'Z5',
+					Z5K1: 'Z1500',
+					Z5K2: {
+						Z1K1: {
+							Z1K1: 'Z7',
+							Z7K1: 'Z885',
+							Z885K1: 'Z1500'
+						},
+						Z1500K1: 'one arg',
+						Z1500K2: 'another arg'
+					}
+				} } );
+
+				wrapper.vm.setErrorMessageCallback( metadata, fallbackMsg, callback );
+				await waitFor( () => expect( callback ).toHaveBeenCalledWith( 'Custom error' ) );
+			} );
 		} );
 	} );
 
-	describe( 'fieldErrors computed property', () => {
-		it( 'should return global errors for local key path', () => {
-			const errors = [
-				{ type: 'global_error', code: 'GLOBAL', message: 'Global error' },
-				{ type: 'local_error', code: 'LOCAL', message: 'Local error' }
-			];
-			store.getErrors = jest.fn().mockImplementation( ( id ) => ( id === 'main.Z2K2' ) ? errors : [] );
+	describe( 'computed properties', () => {
+		describe( 'fieldErrors', () => {
+			it( 'should return global errors for local key path', () => {
+				const errors = [
+					{ type: 'global_error', code: 'GLOBAL', message: 'Global error' },
+					{ type: 'local_error', code: 'LOCAL', message: 'Local error' }
+				];
+				store.getErrors = jest.fn().mockImplementation( ( id ) => ( id === 'main.Z2K2' ) ? errors : [] );
 
-			wrapper.vm.keyPath = 'main.Z2K2';
-			expect( wrapper.vm.fieldErrors ).toEqual( errors );
-		} );
-	} );
-
-	describe( 'hasFieldErrors computed property', () => {
-		it( 'should return true if there are any fieldErrors', () => {
-			const errors = [
-				{ type: 'global_error', code: 'GLOBAL', message: 'Global error' },
-				{ type: 'local_error', code: 'LOCAL', message: 'Local error' }
-			];
-			store.getErrors = jest.fn().mockImplementation( ( id ) => ( id === 'main.Z2K2' ) ? errors : [] );
-
-			wrapper.vm.keyPath = 'main.Z2K2';
-			expect( wrapper.vm.hasFieldErrors ).toBe( true );
+				wrapper.vm.keyPath = 'main.Z2K2';
+				expect( wrapper.vm.fieldErrors ).toEqual( errors );
+			} );
 		} );
 
-		it( 'should return false if there are no fieldErrors', () => {
-			store.getErrors = jest.fn().mockReturnValue( [] );
-			wrapper.vm.keyPath = 'main.Z2K2';
-			expect( wrapper.vm.hasFieldErrors ).toBe( false );
+		describe( 'hasFieldErrors', () => {
+			it( 'should return true if there are any fieldErrors', () => {
+				const errors = [
+					{ type: 'global_error', code: 'GLOBAL', message: 'Global error' },
+					{ type: 'local_error', code: 'LOCAL', message: 'Local error' }
+				];
+				store.getErrors = jest.fn().mockImplementation( ( id ) => ( id === 'main.Z2K2' ) ? errors : [] );
+
+				wrapper.vm.keyPath = 'main.Z2K2';
+				expect( wrapper.vm.hasFieldErrors ).toBe( true );
+			} );
+
+			it( 'should return false if there are no fieldErrors', () => {
+				store.getErrors = jest.fn().mockReturnValue( [] );
+				wrapper.vm.keyPath = 'main.Z2K2';
+				expect( wrapper.vm.hasFieldErrors ).toBe( false );
+			} );
 		} );
-	} );
 
-	describe( 'hasChildErrors', () => {
-		it( 'returns true if there are error keys that are child of current keypath', () => {
-			store.getChildErrorKeys = jest.fn().mockReturnValue( [
-				'main.Z2K2.Z12K1',
-				'main.Z2K2.Z12K1.2'
-			] );
+		describe( 'hasChildErrors', () => {
+			it( 'returns true if there are error keys that are child of current keypath', () => {
+				store.getChildErrorKeys = jest.fn().mockReturnValue( [
+					'main.Z2K2.Z12K1',
+					'main.Z2K2.Z12K1.2'
+				] );
 
-			wrapper.vm.keyPath = 'main.Z2K2';
-			expect( wrapper.vm.hasChildErrors ).toBe( true );
-		} );
+				wrapper.vm.keyPath = 'main.Z2K2';
+				expect( wrapper.vm.hasChildErrors ).toBe( true );
+			} );
 
-		it( 'returns false if there are no error keys that are child of current keypath', () => {
-			store.getChildErrorKeys = jest.fn().mockReturnValue( [] );
+			it( 'returns false if there are no error keys that are child of current keypath', () => {
+				store.getChildErrorKeys = jest.fn().mockReturnValue( [] );
 
-			wrapper.vm.keyPath = 'main.Z2K5';
-			expect( wrapper.vm.hasChildErrors ).toBe( false );
+				wrapper.vm.keyPath = 'main.Z2K5';
+				expect( wrapper.vm.hasChildErrors ).toBe( false );
+			} );
 		} );
 	} );
 } );
