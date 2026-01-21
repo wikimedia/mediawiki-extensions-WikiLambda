d20614fd465f983d050e817afdf23606eca8c2c9
Vaughn Walters <vwalters@wikimedia.org>
2026-01-21 15:29:52 -0600

    selenium: Correctly import default wdio-mediawiki conf before overriding
    Right now, we are not importing the default values from the wdio.conf.js
    before adding our override values to it.  This change is to correctly
    import those values.
    
    Change-Id: Ie15f4b7669c8b5e743f98fdcd43060a0691e5a6d

--- tests/selenium/wdio.conf.mjs
+++ tests/selenium/wdio.conf.mjs
@@ -10,12 +10,15 @@ import { config as wdioDefaults } from 'wdio-mediawiki/wdio-defaults.conf.js';
 export const config = {
 	...wdioDefaults,
 	waitforTimeout: 5000, // milliseconds
-	before() {
+	async before( ...args ) {
+		// Bringing in the default args from the wdio.conf.js in wdio-mediawiki package
+		await wdioDefaults.before?.( ...args );
+
+		// WikiLambda-specific setup
 		browser.addLocatorStrategy( 'ancestor', ( selector, root ) => {
 			const result = root && root.closest( selector );
 			return result ? [ result ] : [];
 		} );
-		browser.setWindowSize( 1280, 1024 );
 	}
 	// Override, or add to, the setting from wdio-mediawiki.
 	// Learn more at https://webdriver.io/docs/configurationfile/
