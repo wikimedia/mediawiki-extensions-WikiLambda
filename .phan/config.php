10239d43ebbfed9b03c1845e8f976c2e3cd5766e
Umherirrender <umherirrender_de.wp@web.de>
2026-04-20 23:13:53 +0200

    build: Use MW_INSTALL_PATH for phan to use core internal extension stub
    Use MW_INSTALL_PATH when set, in case
    this extension is not installed in the core folder (02ace0b8fc).
    
    Change-Id: Ib0c8fdb5e8b9e4c463940f74d19df047ddef2c65

--- .phan/config.php
+++ .phan/config.php
@@ -2,8 +2,13 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$pathMediaWiki = getenv( 'MW_INSTALL_PATH' );
+if ( $pathMediaWiki === false ) {
+	$pathMediaWiki = '../..';
+}
+
 $cfg['autoload_internal_extension_signatures'] = [
-	'memcached' => '../../.phan/internal_stubs/memcached.phan_php',
+	'memcached' => $pathMediaWiki . '/.phan/internal_stubs/memcached.phan_php',
 ];
 
 $cfg['directory_list'] = array_merge(
