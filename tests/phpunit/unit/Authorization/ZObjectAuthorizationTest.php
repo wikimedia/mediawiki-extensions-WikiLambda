d38fb1ba046dd79a9ae4021f816091053e818a91
Umherirrender <umherirrender_de.wp@web.de>
2026-04-25 00:19:24 +0200

    tests: Make PHPUnit data provider static in ZObjectAuthorizationTest
    Create the mocks in the test function
    to have only simple objects on the data provider.
    
    Bug: T410731
    Change-Id: I8883f03b90bc402c8aafb96589bd0aab09117e7c

--- tests/phpunit/unit/Authorization/ZObjectAuthorizationTest.php
+++ tests/phpunit/unit/Authorization/ZObjectAuthorizationTest.php
@@ -59,9 +59,14 @@ class ZObjectAuthorizationTest extends MediaWikiUnitTestCase {
 	 */
 	public function testGetRequiredCreateRights(
 		string $description,
-		ZObjectContent $content,
+		array $contentSpec,
 		array $expectedRights
 	) {
+		if ( $contentSpec[0] === 'functionCallContent' ) {
+			$content = $this->newMockFunctionCallContent( $contentSpec[1], $contentSpec[2] );
+		} else {
+			$content = $this->newMockContent( $contentSpec[1], $contentSpec[2] );
+		}
 		$title = $this->createMock( Title::class );
 		$rights = $this->authorization->getRequiredCreateRights( $content, $title );
 
@@ -78,78 +83,78 @@ class ZObjectAuthorizationTest extends MediaWikiUnitTestCase {
 		);
 	}
 
-	public function provideCreateRights(): iterable {
+	public static function provideCreateRights(): iterable {
 		// ─── User-defined (ZID >= 10000): no wikilambda-create-predefined ───
 
 		yield 'user-defined type (Z4)' => [
 			'user-defined type',
-			$this->newMockContent( ZTypeRegistry::Z_TYPE, 'Z10001' ),
+			[ 'content', ZTypeRegistry::Z_TYPE, 'Z10001' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-type' ],
 		];
 
 		yield 'user-defined function (Z8)' => [
 			'user-defined function',
-			$this->newMockContent( ZTypeRegistry::Z_FUNCTION, 'Z10002' ),
+			[ 'content', ZTypeRegistry::Z_FUNCTION, 'Z10002' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-function' ],
 		];
 
 		yield 'user-defined implementation (Z14)' => [
 			'user-defined implementation',
-			$this->newMockContent( ZTypeRegistry::Z_IMPLEMENTATION, 'Z10003' ),
+			[ 'content', ZTypeRegistry::Z_IMPLEMENTATION, 'Z10003' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-implementation' ],
 		];
 
 		yield 'user-defined tester (Z20)' => [
 			'user-defined tester',
-			$this->newMockContent( ZTypeRegistry::Z_TESTER, 'Z10004' ),
+			[ 'content', ZTypeRegistry::Z_TESTER, 'Z10004' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-tester' ],
 		];
 
 		yield 'user-defined language (Z60)' => [
 			'user-defined language',
-			$this->newMockContent( ZTypeRegistry::Z_LANGUAGE, 'Z10005' ),
+			[ 'content', ZTypeRegistry::Z_LANGUAGE, 'Z10005' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-language' ],
 		];
 
 		yield 'user-defined programming language (Z61)' => [
 			'user-defined programming language',
-			$this->newMockContent( ZTypeRegistry::Z_PROGRAMMINGLANGUAGE, 'Z10006' ),
+			[ 'content', ZTypeRegistry::Z_PROGRAMMINGLANGUAGE, 'Z10006' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-programming' ],
 		];
 
 		yield 'user-defined boolean (Z40)' => [
 			'user-defined boolean',
-			$this->newMockContent( ZTypeRegistry::Z_BOOLEAN, 'Z10007' ),
+			[ 'content', ZTypeRegistry::Z_BOOLEAN, 'Z10007' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-boolean' ],
 		];
 
 		yield 'user-defined unit (Z21)' => [
 			'user-defined unit',
-			$this->newMockContent( ZTypeRegistry::Z_UNIT, 'Z10008' ),
+			[ 'content', ZTypeRegistry::Z_UNIT, 'Z10008' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-unit' ],
 		];
 
 		yield 'user-defined deserialiser (Z46)' => [
 			'user-defined deserialiser',
-			$this->newMockContent( ZTypeRegistry::Z_DESERIALISER, 'Z10009' ),
+			[ 'content', ZTypeRegistry::Z_DESERIALISER, 'Z10009' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-converter' ],
 		];
 
 		yield 'user-defined serialiser (Z64)' => [
 			'user-defined serialiser',
-			$this->newMockContent( ZTypeRegistry::Z_SERIALISER, 'Z10010' ),
+			[ 'content', ZTypeRegistry::Z_SERIALISER, 'Z10010' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-converter' ],
 		];
 
 		yield 'user-defined wikidata enum function call (Z7→Z6884)' => [
 			'user-defined wikidata enum',
-			$this->newMockFunctionCallContent( ZTypeRegistry::Z_WIKIDATA_ENUM, 'Z10011' ),
+			[ 'functionCallContent', ZTypeRegistry::Z_WIKIDATA_ENUM, 'Z10011' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-generic-enum' ],
 		];
 
 		yield 'user-defined other function call (Z7→Z881)' => [
 			'user-defined function call',
-			$this->newMockFunctionCallContent( 'Z881', 'Z10012' ),
+			[ 'functionCallContent', 'Z881', 'Z10012' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-function-call' ],
 		];
 
@@ -157,31 +162,31 @@ class ZObjectAuthorizationTest extends MediaWikiUnitTestCase {
 
 		yield 'predefined type (Z4)' => [
 			'predefined type',
-			$this->newMockContent( ZTypeRegistry::Z_TYPE, 'Z400' ),
+			[ 'content', ZTypeRegistry::Z_TYPE, 'Z400' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-predefined', 'wikilambda-create-type' ],
 		];
 
 		yield 'predefined function (Z8)' => [
 			'predefined function',
-			$this->newMockContent( ZTypeRegistry::Z_FUNCTION, 'Z401' ),
+			[ 'content', ZTypeRegistry::Z_FUNCTION, 'Z401' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-predefined', 'wikilambda-create-function' ],
 		];
 
 		yield 'predefined tester (Z20)' => [
 			'predefined tester',
-			$this->newMockContent( ZTypeRegistry::Z_TESTER, 'Z402' ),
+			[ 'content', ZTypeRegistry::Z_TESTER, 'Z402' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-predefined', 'wikilambda-create-tester' ],
 		];
 
 		yield 'predefined implementation (Z14)' => [
 			'predefined implementation',
-			$this->newMockContent( ZTypeRegistry::Z_IMPLEMENTATION, 'Z403' ),
+			[ 'content', ZTypeRegistry::Z_IMPLEMENTATION, 'Z403' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-predefined', 'wikilambda-create-implementation' ],
 		];
 
 		yield 'predefined wikidata enum function call (Z7→Z6884)' => [
 			'predefined wikidata enum',
-			$this->newMockFunctionCallContent( ZTypeRegistry::Z_WIKIDATA_ENUM, 'Z404' ),
+			[ 'functionCallContent', ZTypeRegistry::Z_WIKIDATA_ENUM, 'Z404' ],
 			[ 'edit', 'wikilambda-create', 'wikilambda-create-predefined', 'wikilambda-create-generic-enum' ],
 		];
 	}
