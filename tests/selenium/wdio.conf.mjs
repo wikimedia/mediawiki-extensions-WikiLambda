b312847ac0d0e0e3b80f30d2b7e3182fb0abe04c
Vaughn Walters <vwalters@wikimedia.org>
2026-05-14 20:37:48 -0500

    selenium: Add Chrome flags for K8s runners
    This is a proof of concept that should be removed here and fixed in wdio-mediawiki if it proves successful.  Running selenium tests in node 24 fails because wdio-mediawiki only sets --no-sandbox/--disable-gpu/--disable-dev-shm-usage inside Docker. This experiment is to see if adding them here allows tests to run in K8s pods too. If so, drop this once wdio-mediawiki handles it. All tests pass on local in node 22 and 24 with this patch added.
    
    Bug: T425968
    Change-Id: I19414c496d71409212816dcb53f956625163102b

--- tests/selenium/wdio.conf.mjs
+++ tests/selenium/wdio.conf.mjs
@@ -7,6 +7,10 @@
 
 import { config as wdioDefaults } from 'wdio-mediawiki/wdio-defaults.conf.js';
 
+// Hack: wdio-mediawiki only adds these in Docker (via /.dockerenv), not K8s pods.
+// This can be removed when the fix is added into wdio-mediawiki
+wdioDefaults.capabilities[ 0 ][ 'goog:chromeOptions' ].args.unshift( '--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage' );
+
 export const config = {
 	...wdioDefaults,
 	waitforTimeout: 5000, // milliseconds
